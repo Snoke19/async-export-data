403f25c6e7215590bd7edea4a5290ed1971803a3
Oleksandr <alex.developer27@outlook.com>
2025-08-04 01:08:24 +0300

    added init data in the db

--- src/main/java/snoke/asyncexportdata/AsyncExportDataApplication.java
+++ src/main/java/snoke/asyncexportdata/AsyncExportDataApplication.java
@@ -1,15 +1,19 @@
 package snoke.asyncexportdata;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
+@RequiredArgsConstructor
 public class AsyncExportDataApplication implements CommandLineRunner {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(AsyncExportDataApplication.class, args);
@@ -17,34 +21,34 @@ public class AsyncExportDataApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        if (userRepository.count() >= 1_000_000) {
-//            System.out.println("Users already seeded.");
-//            return;
-//        }
-//
-//        List<User> users = new ArrayList<>();
-//        for (int i = 0; i < 50000; i++) {
-//            User user = new User(
-//                "User" + i,
-//                "user" + i + "@example.com",
-//                18 + (i % 50),
-//                i % 2 == 0,
-//                LocalDateTime.now().minusDays(i % 365)
-//            );
-//            users.add(user);
-//
-//            if (users.size() == 1000) {
-//                userRepository.saveAll(users);
-//                users.clear();
-//                System.out.println("Inserted 1,000 users...");
-//            }
-//        }
-//
-//        if (!users.isEmpty()) {
-//            userRepository.saveAll(users);
-//            System.out.println("Inserted remaining users.");
-//        }
-//
-//        System.out.println("Finished inserting 50,000 users.");
+        if (userRepository.count() >= 1_000_000) {
+            System.out.println("Users already seeded.");
+            return;
+        }
+
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 50000; i++) {
+            User user = new User(
+                "User" + i,
+                "user" + i + "@example.com",
+                18 + (i % 50),
+                i % 2 == 0,
+                LocalDateTime.now().minusDays(i % 365)
+            );
+            users.add(user);
+
+            if (users.size() == 1000) {
+                userRepository.saveAll(users);
+                users.clear();
+                System.out.println("Inserted 1,000 users...");
+            }
+        }
+
+        if (!users.isEmpty()) {
+            userRepository.saveAll(users);
+            System.out.println("Inserted remaining users.");
+        }
+
+        System.out.println("Finished inserting 50,000 users.");
     }
 }
